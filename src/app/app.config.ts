a0170889a2d3c7454eb97f09b7658b7d048b28e5
agcodes <7748175+agcodes@users.noreply.github.com>
2026-09-19 01:47:22 +0200

    add locale

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideIonicAngular } from '@ionic/angular/standalone';
-
+import { LOCALE_ID } from '@angular/core';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -10,6 +10,19 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(),
-    provideIonicAngular()
-  ]
+    provideIonicAngular(),
+    {
+      provide: LOCALE_ID,
+      useFactory: () => {
+        // Récupère la langue du navigateur (ex: "fr-FR", "en-US")
+        const browserLang = navigator.language;
+        // Angular attend un code de locale comme "fr", "en", "es", etc.
+        const code: string = browserLang.split('-')[0]; // "fr-FR" → "fr"
+        if (code != 'fr' && code != 'en') {
+          return 'en';
+        }
+        return code;
+      },
+    },
+  ],
 };
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { appConfig } from './app/app.config';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+import localeEs from '@angular/common/locales/es';
+import localeEn from '@angular/common/locales/en';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+registerLocaleData(localeFr, 'fr');
+registerLocaleData(localeEs, 'es');
+registerLocaleData(localeEn, 'en');
+
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
